88a548b7c8d069475b35241637fdda746b6f50f4
jennylea28 <jenny.lea168@btinternet.com>
2016-11-15 18:10:42 +0000

    done?

--- Ex3FunctionsCodeGenerator.hs
+++ Ex3FunctionsCodeGenerator.hs
@@ -12,60 +12,54 @@ transFunction :: Function -> [Instr]
 transFunction (Defun fname paramname body)
  = [Define fname] ++ (transExp body regsToUse) ++ [Ret]
 
-
 -- PUSH REGISTERS ONTO THE STACK
 saveRegs :: [Register] -> [Instr]
 saveRegs regsAvailable
  = map (\x-> Mov (Reg x) Push) unused
  where
-   unused = regsToUse \\ regsAvailable
+   unused = allRegs \\ regsAvailable
 
 -- POP REGISTERS FROM THE STACK
 restoreRegs :: [Register] -> [Instr]
 restoreRegs regsAvailable
  = map (\x-> Mov Pop (Reg x)) used
  where
-   used = reverse $ regsToUse \\ regsAvailable
+   used = reverse $ allRegs \\ regsAvailable
 
 
 -- LEAVE RESULT IN FIRST REGISTER IN LIST
 -- PARAMETER VAR X STORED IN REGISTER D0
 transExp :: Exp -> [Register] -> [Instr]
 
-transExp (Const x) (dst:rest)
- = [Mov (ImmNum x)(Reg dst)]
+transExp (Const x) (dest:rest)
+ = [Mov (ImmNum x) (Reg dest)]
 
-transExp (Var x) (dst:rest)
- = [Mov (Reg paramReg)(Reg dst)]
+transExp (Var x) (dest:rest)
+ = [Mov (Reg paramReg) (Reg dest)]
 
 -- SUBTRACT E1 FROM E2
 -- PUT E2 RESULT IN D2
 -- PUT E1 RESULT IN D1
 -- SUB D1 FROM D0
-transExp (Minus e1 e2) regs@(dst:next:rest)
+transExp (Minus e1 e2) (dest:next:rest)
  = if weight e1 > weight e2 then
-  (transExp e1 regs) ++
+  (transExp e1 (dest:next:rest)) ++
   (transExp e2 (next:rest)) ++
-  [Sub (Reg dst) (Reg next)]
+  [Sub (Reg next) (Reg dest)]
   else
-  (transExp e2 (next:dst:rest)) ++
-  (transExp e1 (dst:rest)) ++
-  [Sub (Reg dst) (Reg next)]
-
-transExp (Plus e1 e2) (dst:next:rest)
- = (transExp e2 (next:rest)) ++
-   (transExp e1 (dst:rest)) ++
-   [Add (Reg next) (Reg dst)]
+  (transExp e2 (next:dest:rest)) ++
+  (transExp e1 (dest:rest)) ++
+  [Sub (Reg next) (Reg dest)]
 
 -- MOVE THE VARIABLE YOU'RE PASSING IN 
 -- PASSED IN GOES INTO D0
 -- FUNCTION RETURNS ARGUMENT IN D1
-transExp (Apply string e) regs@(dst:rest)
+transExp (Apply string e) regs@(dest:next:rest)
   = (saveRegs regs) ++ 
-    (transExp e regsToUse) ++
-    [Mov (Reg dst) (Reg paramReg)] ++ 
+    (transExp e regs) ++
+    [Mov (Reg dest) (Reg paramReg)] ++ 
     [Jsr string] ++ 
-    [Mov (Reg resultReg) (Reg D0)] ++ 
+    [Mov (Reg paramReg) (Reg dest)] ++ 
     (restoreRegs regs)
 
 weight :: Exp -> Int
@@ -76,5 +70,5 @@ weight (Minus e1 e2)
  where
   cost1 = max (weight e1) ((weight e2) + 1)
   cost2 = max ((weight e1) + 1) (weight e2)
-weight (Apply string e) = 1
+weight (Apply string e) = 1 + (weight e)
 
